5013b1b29f603707c33a9bdb255927a4ada81e4b
lizasilent <elizavetad@yandex.ru>
2021-12-07 17:58:24 +0300

    start

--- src/App.js
+++ src/App.js
@@ -2,36 +2,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="app-header">
       </header>
-      <main className="Main">
-      <div className="Wrapper">
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
-   <div className="Wrapper-cell"></div>
+      <main className="main">
+      <div className="wrapper">
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
+   <div className="wrapper-cell"></div>
 </div>
 
       </main>
