977f9c5d0fb3fa79cc07fdfef9ebdae9bda3b888
lizasilent <elizavetad@yandex.ru>
2021-12-07 16:06:19 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <main className="Main">
+      <div className="Wrapper">
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+   <div className="Wrapper-cell"></div>
+</div>
+
+      </main>
+        
+    </div>
+  );
+}
+
+export default App;
