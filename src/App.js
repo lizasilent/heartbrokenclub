b82a8ffe9d77eb509c463ac48f06d8c87d663f8f
lizasilent <elizavetad@yandex.ru>
2022-01-08 16:33:12 +0300

    popup done

--- src/App.js
+++ src/App.js
@@ -3,39 +3,54 @@ import './App.css';
 function App() {
   return (
     <div className="app">
+
       <header className="app-header">
       </header>
       <main className="main">
       <div className="wrapper">
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-   <div className="wrapper-cell"></div>
-</div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          <div className="wrapper-cell"></div>
+          </div>
+          </main>
+<footer>Created by liza__silent</footer>
+          
 
-      </main>
-        
+<div className="popup popup_is-open">
+            <div className="popup__form">
+                <button className="popup__close-btn" type="button"/>
+                <div className="popup__img"></div>
+                <div className="popup__text-content"> 
+                <p className="popup__header">Ремез А.</p>
+                  <p className="popup__header">Прогулка по прямой : шесть историй о любви</p>
+                  <p className="popup__description">Первая любовь — радость или тяжёлое испытание? За этим сильным чувством стоит столько переживаний и страхов... На что придётся пойти ради неё? А если будут против родители или засмеют друзья? А что, если мы больше не увидимся, кроме как этим летом на море? А вдруг это всё злая шутка одноклассников?..
+Именно этим чувствам и посвящён сборник рассказов Анны Ремез. Шесть историй о любви. Первой, подростковой, очень непростой и очень разной.
+Анна Ремез — прозаик, переводчик, финалист премий «Книгуру», премии имени Владислава Крапивина, лауреат премии «Новая детская книга». Ольга Явич — художник книги, руководитель мастерской для детей «Злой волк».</p>
+            </div>
+            </div>
+        </div>
     </div>
   );
 }
