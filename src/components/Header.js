ceea0e98cdf9ccd296de16fa613a406fdaa9f002
Liza_silent <elizavetad@ya.ru>
2022-02-11 15:16:51 +0300

    logo link added3

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="header">
       <div className="header__content">
-        <a href="https://www.podpisnie.ru/" target="_blank"><div className="header__logo"></div></a>
+        <a href="https://www.podpisnie.ru/" target="blank"><div className="header__logo"></div></a>
         <p className="header__text">x</p>
         <p className="header__text header__text_style">heartbrokenclub</p>
       </div>
